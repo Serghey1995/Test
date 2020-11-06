f5c277a1ec8857a15b5fb3de95353d8df23377ea
Serghey1995 <smoskalu88@gmail.com>
2020-11-06 19:33:31 +0200

    git add

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> dublicareArr = new ArrayList<Integer>();
+        dublicareArr.add(1);
+        dublicareArr.add(1);
+        dublicareArr.add(2);
+        dublicareArr.add(2);
+        dublicareArr.add(3);
+        dublicareArr.add(3);
+        ArrayList<Integer> sortedArr = new ArrayList<Integer>();
+        for (Integer n : dublicareArr) {
+            if (!sortedArr.contains(n)) {
+                if (n % 2 == 0) {
+                    sortedArr.add(n);
+                }
+            }
+        }
+        System.out.println(dublicareArr);
+        System.out.println(sortedArr);
+    }
+}
