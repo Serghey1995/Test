44799c4e14e4a6830e2c9642ce9ca45dbd71a9ba
Serghey1995 <smoskalu88@gmail.com>
2020-11-12 21:01:11 +0200

    сумма положительных чисел массива

---
+++ src/com/company/scratch.java
@@ -0,0 +1,24 @@
+package com.company;
+
+class Scratch {
+    public static void main(String[] args) {
+        int[] nums2 = { 1,2,3,4,5 };
+        int sum=0;
+        for(int i=0; i<nums2.length; i++) {
+            if (nums2[i]>0) {
+                sum +=nums2[i];
+            }
+        }
+        System.out.println(sum);
+    }
+}
+
+
+
+
+
+// str 1 = "java" str 2 = "1.8" str 3 = " "
+// str1+str3+str2
+// s1 + str2
+// s2
+// sb Java
